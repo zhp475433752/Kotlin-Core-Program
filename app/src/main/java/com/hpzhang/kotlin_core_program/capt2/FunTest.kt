24475b7c74e2280c5b57dca87a86d1db6522de61
hpzhang <zhp475433752@163.com>
2021-03-27 23:14:57 +0800

    添加方法调用

--- app/src/main/java/com/hpzhang/kotlin_core_program/capt2/FunTest.kt
+++ app/src/main/java/com/hpzhang/kotlin_core_program/capt2/FunTest.kt
@@ -4,23 +4,9 @@ import java.util.Collections.emptyList
 
 /**
  * Created by hpzhang on 3/27/21.
+ * 函数、高阶函数、lambda
  */
 public class FunTest {
-    // 高阶函数：以其他函数作为参数或者返回值的函数
-    private fun filterCountries(
-        countries: List<Country>,
-        bigCountry: (Country) -> Boolean    // 这是一个
-    ): List<Country> {
-        for (country in countries) {
-            if (bigCountry(country)) {
-                println("大国：${country.name}")
-            } else {
-                println("小国：${country.name}")
-            }
-        }
-        return emptyList()
-    }
-
     /**
      * 真正调用的地方
      */
@@ -39,11 +25,43 @@ public class FunTest {
         })
 
         // 调用方式3：lambda表达式(与匿名函数一样，是一种函数字面量)
-        filterCountries(list, {
-            country -> "中国" == country.name
+        // lambda表达式可以放在（）外面
+        filterCountries(list, { country ->
+            "中国" == country.name
         })
+
+        // 调用方式4：和3是一样的，只不过lambda表达式放到了()外面
+        filterCountries(list) {
+            "中国" == it.name
+        }
+
+        listOf(1, 2, 3).forEach { foo(it) }
+
+
+    }
+    // 高阶函数：以其他函数作为参数或者返回值的函数
+    private fun filterCountries(
+        countries: List<Country>,
+        bigCountry: (Country) -> Boolean    // 这是一个函数类型参数
+    ): List<Country> {
+        for (country in countries) {
+            if (bigCountry(country)) {
+                println("大国：${country.name}")
+            } else {
+                println("小国：${country.name}")
+            }
+        }
+        return emptyList()
     }
 
+    fun foo(int: Int): Int {
+        println("foo = $int")
+        return 1
+    }
+
+    fun foo2(x: Int): (Int) -> Int {
+        return { y: Int -> x + y }
+    }
 }
 
 data class Country(val name: String)
--- app/src/main/java/com/hpzhang/kotlin_core_program/capt2/LamTest.kt
+++ app/src/main/java/com/hpzhang/kotlin_core_program/capt2/LamTest.kt
@@ -4,4 +4,10 @@ package com.hpzhang.kotlin_core_program.capt2
  * Created by hpzhang on 3/27/21.
  */
 class LamTest {
+    fun t() {
+        listOf(1,2).forEach { foo(it) }
+    }
+    fun foo(int: Int) {
+        println(int)
+    }
 }
